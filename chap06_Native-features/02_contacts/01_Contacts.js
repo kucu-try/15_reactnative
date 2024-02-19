9f9f42828ef7f69d814f50db9fe9a6bf9af5f09e
kucu-try <baekfive@gmail.com>
2024-02-19 18:20:39 +0900

    reactNative

--- chap06_Native-features/02_contacts/01_Contacts.js
+++ chap06_Native-features/02_contacts/01_Contacts.js
@@ -1,75 +1,73 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import * as Contacts from 'expo-contacts';
 import { FlatList, Linking, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const  ContactsView =()=>{
 
-    const [contacts, setContacts] =useState([]);
+const ContactsView = () => {
+  const [contacts, setContacts] = useState([]);
 
-    useEffect(()=>{
-        (async () => {
-            const {status} = await Contacts.requestPermissionsAsync();
-            if (status === "granted") { 
-                const {data} = await Contacts.getContactsAsync({
-                    fields : [Contacts.Fields.Name, Contacts.Fields.PhoneNumbers]
-                });
+  useEffect(()=>{
+    (async () =>{
+      const {status} = await Contacts.requestPermissionsAsync();
+      if(status === "granted"){  //권한체크
+        const {data} = await Contacts.getContactsAsync({
+          fields : [Contacts.Fields.Name, Contacts.Fields.PhoneNumbers]
+        });
 
-                if (data.length > 0) {
-                    setContacts(data)
-                }
-            }
-        })();
-    },[]);
+        if(data.length > 0){
+          setContacts(data)
+        }
+      }
+    })();
+  },[]);
 
-    const handleCall = (phoneNumbers) =>{
-        Linking.openURL(`tel:${phoneNumbers}`);
-    }
+  const handleCall = (phoneNumbers) => {
+    Linking.openURL(`tel:${phoneNumbers}`);
+  }
 
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>내 연락처</Text>
-            <FlatList
-                data={contacts}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => handleCall(item.phoneNumbers[0].number)}>
-                        <View style={styles.contactItem}>
-                            <Text style={styles.contactName}>{item.name}</Text>
-                            {item.phoneNumbers && item.phoneNumbers.length > 0 && (
-                                <Text style={styles.contactNumber}>{item.phoneNumbers[0].number}</Text>
-                            )}
-                        </View>
-                    // </TouchableOpacity>
-                )}
-                keyExtractor={(item) => item.id}
-            />
-        </View>
-    );
-
-                            }
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}>내 연락처</Text>
+        <FlatList
+            data={contacts}
+            renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => handleCall(item.phoneNumbers[0].number)}>
+                    <View style={styles.contactItem}>
+                        <Text style={styles.contactName}>{item.name}</Text>
+                        {item.phoneNumbers && item.phoneNumbers.length > 0 && (
+                            <Text style={styles.contactNumber}>{item.phoneNumbers[0].number}</Text>
+                        )}
+                    </View>
+               </TouchableOpacity>
+            )}
+            keyExtractor={(item) => item.id}
+        />
+    </View>
+);
+}
 export default ContactsView;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 40,
-        paddingHorizontal: 20,
-        backgroundColor: '#fff',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-    },
-    contactItem: {
-        marginBottom: 10,
-        borderWidth:2
-    },
-    contactName: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    contactNumber: {
-        fontSize: 16,
-    },
+  container: {
+      flex: 1,
+      paddingTop: 40,
+      paddingHorizontal: 20,
+      backgroundColor: '#fff',
+  },
+  title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+  },
+  contactItem: {
+      marginBottom: 10,
+      borderWidth:2
+  },
+  contactName: {
+      fontSize: 18,
+      fontWeight: 'bold',
+  },
+  contactNumber: {
+      fontSize: 16,
+  },
 });
